b10e2847a420cc8d73883c634bb5adf0af6d75bd
SIDDHARTH-S-001 <73553742+SIDDHARTH-S-001@users.noreply.github.com>
2022-10-31 22:05:45 +0530

    Add files via upload

---
+++ live_classes/src/load_image.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import os
+
+
+
+class LoadImage(object):
+
+    def __init__(self):
+    
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.camera_callback)
+        self.bridge_object = CvBridge()
+
+    def camera_callback(self,data):
+        try:
+            # We select bgr8 because its the OpenCV encoding by default
+            cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+        example_path = '/home/ssiddharth/catkin_ws/src/notebook_ws/images/Course_images/drone.jpg'    
+        img = cv2.imread(example_path)
+        os.chdir('/home/ssiddharth/catkin_ws/src/live_classes/src/')
+        cv2.imwrite('drone.jpg',cv_image)
+        cv2.imshow('image',img)
+        cv2.waitKey(0)
+
+
+
+def main():
+    load_image_object = LoadImage()
+    rospy.init_node('load_image_node', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
